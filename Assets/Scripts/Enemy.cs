02d4c2df9a79bd12a4579b3a2831488bb331abbe
KaleBale <KabyangaPaul@gmail.com>
2026-01-18 22:19:23 +0300

    Update Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,85 +1,141 @@
+using System.Numerics;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
-using System;
-using System.Security.AccessControl;
+
 
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
 public class Enemy : MonoBehaviour
-{
-    [SerializeField] float speed = 3;
-    [SerializeField] Vector3 target = Vector3.zero;
+{    
+    [Header("Target Settings")]
+    // [SerializeField] float speed = 3;
+    // [SerializeField] UnityEngine.Vector3 target = UnityEngine.Vector3.zero;
+    [SerializeField] Transform target;
     [SerializeField] string bulletTag = "Bullet";
     [SerializeField] string targetTag = "TrashPile";
 
-    [SerializeField] int curvepoints = 3;// number of points in curve
-    [SerializeField] float curveradius = 2f; //offset off path
+   // [SerializeField] int curvepoints = 3;// number of points in curve
+   // [SerializeField] float curveradius = 2f; //offset off path
 
-    private Rigidbody rb;
-    private Vector3[] pathpoints;
-    private int currentpoint = 0;
+
+    [Header("Target Settings")]
+
+    [SerializeField] float wanderradius = 6f;
+
+    [SerializeField] float repathdistance = 1f;
+
+    private NavMeshAgent agent;
+    private UnityEngine.Vector3 currentgoal;
+
+    // private Rigidbody rb;
+    // private Vector3[] pathpoints;
+    // private int currentpoint = 0;
+
+
+
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        GeneratePath();
+        //rb = GetComponent<Rigidbody>();
+
+        //GeneratePath();
+
+        agent = GetComponent<NavMeshAgent>();
+
+        agent.stoppingDistance = 0.5f;
+        agent.autoBraking = false;
     }
 
-    void FixedUpdate()
-    // {
-    //     // Get the direction to the target (trash)
-    //     Vector3 toTarget = target - transform.position;
-    //     toTarget.y = 0;
+    // void FixedUpdate()
+    // // {
+    // //     // Get the direction to the target (trash)
+    // //     Vector3 toTarget = target - transform.position;
+    // //     toTarget.y = 0;
         
-    //     // Look and move towards the target
-    //     transform.rotation = Quaternion.LookRotation(toTarget.normalized, Vector3.up);
-    //     rb.linearVelocity = toTarget.normalized * speed;
+    // //     // Look and move towards the target
+    // //     transform.rotation = Quaternion.LookRotation(toTarget.normalized, Vector3.up);
+    // //     rb.linearVelocity = toTarget.normalized * speed;
 
-     {
-        if (pathpoints == null || pathpoints.Length == 0) return;
+    //  {
+    //     if (pathpoints == null || pathpoints.Length == 0) return;
 
-        Vector3 nextPoint = pathpoints[currentpoint];
-        Vector3 dir = nextPoint - transform.position;
-        dir.y = 0;
-        Vector3 moveDir = dir.normalized;
+    //     Vector3 nextPoint = pathpoints[currentpoint];
+    //     Vector3 dir = nextPoint - transform.position;
+    //     dir.y = 0;
+    //     Vector3 moveDir = dir.normalized;
 
-        // Look and move toward next point
-        transform.rotation = Quaternion.LookRotation(moveDir, Vector3.up);
-        rb.linearVelocity = moveDir * speed;
+    //     // Look and move toward next point
+    //     transform.rotation = Quaternion.LookRotation(moveDir, Vector3.up);
+    //     rb.linearVelocity = moveDir * speed;
 
-        // Check if reached waypoint
-        if (Vector3.Distance(transform.position, nextPoint) < 0.1f)
+    //     // Check if reached waypoint
+    //     if (Vector3.Distance(transform.position, nextPoint) < 0.1f)
+    //     {
+    //         currentpoint++;
+    //         if (currentpoint >= pathpoints.Length)
+    //         {
+    //             // Stop at target
+    //             rb.linearVelocity = Vector3.zero;
+    //         }
+    //     }
+    // //}
+
+
+    // }
+
+    // private void GeneratePath()
+    // {
+    //     pathpoints = new Vector3[curvepoints + 1];
+    //     Vector3 start = transform.position;
+    //     Vector3 end = target;
+
+    //     for (int i = 0; i < curvepoints; i++)
+    //     {
+    //         float t = (i + 1f) / (curvepoints + 1f);
+    //         Vector3 point = Vector3.Lerp(start, end, t);
+
+    //         // Offset sideways to create curve
+    //         Vector3 sideways = Vector3.Cross(Vector3.up, (end - start).normalized);
+    //         point += sideways * UnityEngine.Random.Range(-curveradius, curveradius);
+
+    //         pathpoints[i] = point;
+    //     }
+    //     pathpoints[curvepoints] = end;
+
+    // }
+
+     void Update()
+    {
+        if (!agent.pathPending && agent.remainingDistance < repathdistance)
         {
-            currentpoint++;
-            if (currentpoint >= pathpoints.Length)
-            {
-                // Stop at target
-                rb.linearVelocity = Vector3.zero;
-            }
+            PickNewGoal();
         }
-    //}
 
-
+        if (UnityEngine.Vector3.Distance(transform.position, target.position) < 0.5f)
+        {
+            Destroy(gameObject);
+        }
+        
     }
 
-    private void GeneratePath()
+    void PickNewGoal()
     {
-        pathpoints = new Vector3[curvepoints + 1];
-        Vector3 start = transform.position;
-        Vector3 end = target;
+        UnityEngine.Vector3 randomOffest = Random.insideUnitSphere * wanderradius; // pick a random point near the target
+        randomOffest.y = 0f; // make sure its on the ground
 
-        for (int i = 0; i < curvepoints; i++)
+        UnityEngine.Vector3 candidate = target.position + randomOffest;
+
+        if (NavMesh.SamplePosition(candidate,out NavMeshHit hit, wanderradius, NavMesh.AllAreas))
         {
-            float t = (i + 1f) / (curvepoints + 1f);
-            Vector3 point = Vector3.Lerp(start, end, t);
-
-            // Offset sideways to create curve
-            Vector3 sideways = Vector3.Cross(Vector3.up, (end - start).normalized);
-            point += sideways * UnityEngine.Random.Range(-curveradius, curveradius);
-
-            pathpoints[i] = point;
+            currentgoal = hit.position;
+            agent.SetDestination(currentgoal);
+        } else
+        {
+            agent.SetDestination(target.position);
         }
-        pathpoints[curvepoints] = end;
     }
 
     void OnCollisionEnter(Collision collision)
